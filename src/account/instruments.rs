053ab80795fbd47a0cdca05160c95bbef53bb1c4
Zach Blankenship <zach.blankenship@gmail.com>
2017-05-25 19:43:44 -0400

    Add account instrument list retrieval
    Adds the ability to fetch the account instruments endpoint:
    
    ```
    /v3/accounts/{id}/instruments
    ```
    
    Resulting in a `Vec<Instrument>` reflective of the tradable currencies
    for the account and their attributes

---
+++ src/account/instruments.rs
@@ -0,0 +1,57 @@
+#[derive(Deserialize)]
+pub enum InstrumentType {
+    // Currency
+    CURRENCY,
+    //Contract For Difference
+    CFD,
+    // METAL
+    METAL
+}
+
+#[derive(Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Instrument {
+    // The name of the Instrument
+    pub name: String,
+    // The type of the Instrument
+    #[serde(rename = "type")]
+    pub instrument_type: InstrumentType,
+    // The display name of the Instrument
+    pub display_name: String,
+    // The location of the “pip” for this instrument. The decimal position of
+    // the pip in this Instrument’s price can be found at 10 ^ pipLocation e.g.
+    // -4 pipLocation results in a decimal pip position of 10 ^ -4 = 0.0001.
+    pub pip_location: i32,
+    // The number of decimal places that should be used to display prices for
+    // this instrument. e.g. a displayPrecision of 5 would result in a price of
+    // “1” being displayed as “1.00000”
+    pub display_precision: i32,
+    // The amount of decimal places that may be provided when specifying the
+    // number of units traded for this instrument.
+    pub trade_units_precision: i32,
+    // The smallest number of units allowed to be traded for this instrument.
+    pub minimum_trade_size: f32,
+    // The maximum trailing stop distance allowed for a trailing stop loss
+    // created for this instrument. Specified in price units.
+    pub maximum_trailing_stop_distance: f32,
+    // The minimum trailing stop distance allowed for a trailing stop loss
+    // created for this instrument. Specified in price units.
+    pub minimum_trailing_stop_distance: f32,
+    // The maximum position size allowed for this instrument. Specified in
+    // units.
+    pub maximum_position_size: f32,
+    // The maximum units allowed for an Order placed for this instrument.
+    // Specified in units.
+    pub maximum_order_units: f32,
+    // The margin rate for this instrument.
+    pub margin_rate: f32
+}
+
+#[derive(Deserialize)]
+pub struct AccountInstruments {
+    // The requested list of instruments.
+    pub instruments: Vec<Instrument>,
+    // The ID of the most recent Transaction created for the Account.
+    #[serde(rename = "lastTransactionID")]
+    pub last_transaction_id: String
+}
--- src/account/mod.rs
+++ src/account/mod.rs
@@ -1,4 +1,5 @@
 pub mod details;
+pub mod instruments;
 pub mod summary;
 
 use serde_json;
@@ -8,6 +9,8 @@ use self::details::AccountDetails;
 use self::details::Details;
 use self::summary::AccountSummary;
 use self::summary::Summary;
+use self::instruments::AccountInstruments;
+use self::instruments::Instrument;
 
 fn none() -> Option<&'static Client<'static>> { None }
 
@@ -28,16 +31,25 @@ pub struct Account<'a> {
 impl <'a>Account<'a> {
     pub fn details(&self) -> Details {
         let input = self.client().get(format!("accounts/{}", self.id).as_str());
-        let mut result: AccountDetails = serde_json::from_str(&input).unwrap();
+        let result: AccountDetails = serde_json::from_str(&input).unwrap();
 
         result.account
+    }
+
+    pub fn instruments(&self) -> Vec<Instrument> {
+        let input = self.client().get(
+            format!("accounts/{}/instruments", self.id).as_str()
+        );
+        let result: AccountInstruments = serde_json::from_str(&input).unwrap();
+
+        result.instruments
     }
 
     pub fn summary(&self) -> Summary {
         let input = self.client().get(
             format!("accounts/{}/summary", self.id).as_str()
         );
-        let mut result: AccountSummary = serde_json::from_str(&input).unwrap();
+        let result: AccountSummary = serde_json::from_str(&input).unwrap();
 
         result.account
     }
@@ -61,7 +73,8 @@ mod tests {
 
         let client = Client::new(&url, &key);
         let accounts = client.accounts();
-        let account = accounts.first().unwrap();
+
+        let account = accounts.first().unwrap().clone();
         let details = account.details();
 
         assert_eq!(details.alias.unwrap(), "Testv20")
@@ -75,9 +88,26 @@ mod tests {
 
         let client = Client::new(&url, &key);
         let accounts = client.accounts();
-        let account = accounts.first().unwrap();
-        let details = account.summary();
 
-        assert_eq!(details.alias.unwrap(), "Testv20")
+        let account = accounts.first().unwrap().clone();
+        let summary = account.summary();
+
+        assert_eq!(summary.alias.unwrap(), "Testv20")
+    }
+
+    #[test]
+    fn it_can_read_account_instruments() {
+        let url = env::var("OANDA_API_URL").unwrap();
+        let key = env::var("OANDA_API_KEY").unwrap();
+        let account_id = env::var("OANDA_TEST_ACCOUNT_ID").unwrap();
+
+        let client = Client::new(&url, &key);
+        let accounts = client.accounts();
+
+        let account = accounts.first().unwrap().clone();
+        let instruments = account.instruments();
+        // DKK just hapepns to the be the first one, the result here is a list
+        // of all USD_* tradable currencies because the test account is USD
+        assert_eq!(instruments.first().unwrap().name, "USD_DKK")
     }
 }
