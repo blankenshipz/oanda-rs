b6424b3a218988b85329f7b29acdaa273a624054
Michael van Niekerk <mike@agri-io.co.za>
2023-05-01 11:52:03 +0200

    Candlestick prices encoded as strings, not f32

--- src/instrument/candlestick_data.rs
+++ src/instrument/candlestick_data.rs
@@ -3,12 +3,12 @@ use serde_derive::Deserialize;
 #[derive(Deserialize, Clone, Debug)]
 pub struct CandlestickData {
     /// The first (open) price in the time-range represented by the candlestick.
-    pub o: f32,
+    pub o: String,
     /// The highest price in the time-range represented by the candlestick.
-    pub h: f32,
+    pub h: String,
     /// The lowest price in the time-range represented by the candlestick.
-    pub l: f32,
+    pub l: String,
     /// The last (closing) price in the time-range represented by the
     /// candlestick.
-    pub c: f32,
+    pub c: String,
 }
